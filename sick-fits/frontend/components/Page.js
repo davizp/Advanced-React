a5a2aa72159c8da3f29107056fb944bca85c95d4
David Nuñez <dh_nz@hotmail.com>
2019-08-29 17:40:54 -0600

    06 - Themes and Layout with Styled Components. First Commit

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const StylePage = styled.div`
+  background: white;
+  color:  ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  background: ${props => props.theme.red};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends Component {
 
@@ -8,12 +31,14 @@ class Page extends Component {
     const { Component } =  this.props;
 
     return (
-      <div>
-        <Meta />
-        <p>Heey I'm the page component</p>
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StylePage>
+          <Meta />
+          <p>Heey I'm the page component</p>
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StylePage>
+        </ThemeProvider>
     );
   }
 }
