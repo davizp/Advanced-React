cfe5882afbde3cbddefa6f15deb8f7374982b34a
David Nuñez <dh_nz@hotmail.com>
2020-03-08 21:35:07 -0600

    13 - Our first Query and Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    // create a dog!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+
+    return newDog;
+  },
+};
 
 module.exports = mutations;
