41f12606e0be8c11b10a04266e6d9d4a2d0e9012
David Nuñez <dh_nz@hotmail.com>
2020-03-15 14:39:58 -0600

    18 - Uploading Images

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -4,7 +4,6 @@ import { Mutation } from 'react-apollo';
 import Router from 'next/router';
 import Form from './styles/Form';
 import Error from '../components/ErrorMessage';
-import formatMoney from '../lib/formatMoney';
 
 export const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -54,8 +53,32 @@ class CreateItem extends Component {
     });
   };
 
+  uploadFile = async event => {
+    const files = event.target.files;
+    const data = new FormData();
+
+    // data.append('cloud_name', 'dvhponpbr');
+    data.append('upload_preset', 'sickfits');
+    data.append('file', files[0]);
+
+    const res = await fetch(
+      '//api.cloudinary.com/v1_1/dvhponpbr/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+
+    const file = await res.json();
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
-    const { title, price, description } = this.state;
+    const { title, image, price, description } = this.state;
 
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -74,6 +97,24 @@ class CreateItem extends Component {
                   onChange={this.handleChange}
                   required
                 />
+              </label>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  onChange={this.uploadFile}
+                  required
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
+                  />
+                )}
               </label>
               <label htmlFor="price">
                 Price
