880692b2d98852dab09536934a74cd2297213b7d
David Nuñez <dh_nz@hotmail.com>
2020-06-08 19:54:40 -0600

    29 - Backend Password Reset Flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+
+const randomBytesPromisified = promisify(randomBytes);
 
 const mutations = {
   async createItem(parent, args, context, info) {
@@ -111,6 +115,74 @@ const mutations = {
     context.response.clearCookie('token');
 
     return { message: 'done' };
+  },
+  async requestReset(parent, args, context, info) {
+    const { email } = args;
+
+    // check if is a real user
+    const user = await context.db.query.user({ where: { email } });
+
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // set a reset token and expiry on that user
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 1000 * 60 * 60; // 1 hour from now
+    const res = await context.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetTokenExpiry }
+    });
+
+    console.log('res', res);
+
+    // email them the reset token
+
+    return { message: 'done' };
+  },
+  async resetPassword(parent, args, context, info) {
+    const { password, confirmPassword, resetToken } = args;
+    // check if the passwords match
+    if (password !== confirmPassword) {
+      throw new Error('Your passwords don\'t match!');
+    }
+
+    // check if its a legit reset token
+    // check if its expired
+    const [user] = await context.db.query.users({
+      where: {
+        resetToken,
+        resetTokenExpiry_gte: Date.now() - 1000 * 60 * 60
+      }
+    });
+
+    if (!user) {
+      throw new Error('This token is either invalid or expired!');
+    }
+
+    // hash their new password
+    const newPassword = await bcrypt.hash(args.password, 10);
+
+    // save the new password and remove resetToken
+    const updatedUser = await context.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password: newPassword,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    });
+
+    // Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+
+    // SET JWT cookie
+    context.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    // return the user
+    return updatedUser;
   }
 };
 
